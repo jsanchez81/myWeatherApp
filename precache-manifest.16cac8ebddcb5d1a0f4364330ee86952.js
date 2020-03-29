92811e1eb52a14bcdf0fb9d49850ce1ee2a806ac
jsanchez81 <alex_sl7@icloud.com>
2020-03-29 13:40:38 +0200

    Updates

---
+++ precache-manifest.16cac8ebddcb5d1a0f4364330ee86952.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14219f119f107fc182df4d462892f1c",
+    "url": "/myWeatherApp/index.html"
+  },
+  {
+    "revision": "3633fa11513bb74f8fae",
+    "url": "/myWeatherApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ffdd91bab94eb87ce9d",
+    "url": "/myWeatherApp/static/js/2.c0cbe94b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myWeatherApp/static/js/2.c0cbe94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3633fa11513bb74f8fae",
+    "url": "/myWeatherApp/static/js/main.d4eba633.chunk.js"
+  },
+  {
+    "revision": "18bef040bf1661ee5001",
+    "url": "/myWeatherApp/static/js/runtime-main.6a769d01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myWeatherApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
